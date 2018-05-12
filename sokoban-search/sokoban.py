723ff013d178db6df0a8eb627b0fd3a7cdcbdbe1
franciscoafonsoo <afonso.fcul@gmail.com>
2018-05-12 17:25:02 +0100

    Feito - 2.3 Leitura de puzzle de ficheiro

--- sokoban-search/sokoban.py
+++ sokoban-search/sokoban.py
@@ -2,47 +2,74 @@
 from search import *
 
 
-class ProblemaTeoricas_1(Problem):
-    grafo = {'I':{'A':2,'B':5},
-             'A':{'C':2,'D':4,'I':2},
-             'B':{'D':1,'F':5,'I':5},
-             'C':{},
-             'D':{'C':3,'F':2},
-             'F':{}}
+class Sokoban(Problem):
+    def __init__(self, initial, goal=None):
+        """The constructor specifies the initial state, and possibly a goal
+        state, if there is a unique goal.  Your subclass's constructor can add
+        other arguments."""
 
-    def actions(self, estado):
-        sucessores = self.grafo[estado].keys()  # métodos keys() devolve a lista das chaves do dicionário
-        accoes = list(map(lambda x: "ir de {} para {}".format(estado, x), sucessores))
-        return accoes
+        self.initial = initial
+        self.goal = goal
 
-    def result(self, estado, accao):
-        """Assume-se que uma acção é da forma 'ir de X para Y'
-        """
-        return accao.split()[-1]
+    def actions(self, state):
+        """Return the actions that can be executed in the given
+        state. The result would typically be a list, but if there are
+        many actions, consider yielding them one at a time in an
+        iterator, rather than building them all at once."""
+        raise NotImplementedError
+
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+        raise NotImplementedError
+
+    def goal_test(self, state):
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal or checks for state in self.goal if it is a
+        list, as specified in the constructor. Override this method if
+        checking against a single self.goal is not enough."""
+        if isinstance(self.goal, list):
+            return is_in(state, self.goal)
+        else:
+            return state == self.goal
 
     def path_cost(self, c, state1, action, state2):
-        return c + self.grafo[state1][state2]
+        """Return the cost of a solution path that arrives at state2 from
+        state1 via action, assuming cost c to get up to state1. If the problem
+        is such that the path doesn't matter, this function will only look at
+        state2.  If the path does matter, it will consider c and maybe state1
+        and action. The default method costs 1 for every step in the path."""
+        return c + 1
 
-    def h1(self, no):
-        """Uma heurística é uma função de um estado.
-        Nesta implementação, é uma função do estado associado ao nó
-        (objecto da classe Node) fornecido como argumento.
-        """
-        h = {'I': 7, 'A': 2, 'B': 3, 'C': 1, 'D': 5, 'F': 0}
-        return h[no.state]
-
-    def h2(self, no):
-        """Uma heurística é uma função de um estado.
-        Nesta implementação, é uma função do estado associado ao nó
-        (objecto da classe Node) fornecido como argumento.
-        """
-        h = {'I': 7, 'A': 8, 'B': 11, 'C': 0, 'D': 5, 'F': 0}
-        return h[no.state]
+    def value(self, state):
+        """For optimization problems, each state has a value.  Hill-climbing
+        and related algorithms try to maximize this value."""
+        raise NotImplementedError
 
 
-prob1 = ProblemaTeoricas_1('I','F')
+def import_sokoban_file(filename):
+    """
+    2.3 Leitura de puzzle de ficheiro
 
-res_astar = astar_search(prob1,prob1.h2)
-print(res_astar.solution(),res_astar.path_cost)
+    - Le o ficheiro linha a linha, e converte cada linha para uma lista de chars
+    - Remove newline char
+    - Verifica se cada linha tem o mesmo comprimento
 
-# implementar h2
+    :return: matrix com as linhas lidas do ficheiro
+    """
+    matrix = list()
+    with open(filename) as file:
+        for line in file:
+            matrix.append(list(line.rstrip('\n')))
+
+    len_first = len(matrix[0])
+    return matrix if all(len(i) == len_first for i in matrix) else False
+
+
+# importar os 3 ficheiros e testar
+puzzle1 = import_sokoban_file('puzzles/puzzle1.txt')
+puzzle2 = import_sokoban_file('puzzles/puzzle2.txt')
+puzzle3 = import_sokoban_file('puzzles/puzzle3.txt')
+
+Sokoban(puzzle1)
