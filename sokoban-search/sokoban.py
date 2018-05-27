02f86c68fa936698cb74680bba1f4f685ca0f3db
franciscoafonsoo <afonso.fcul@gmail.com>
2018-05-27 22:46:49 +0100

    sokoban comments

--- sokoban-search/sokoban.py
+++ sokoban-search/sokoban.py
@@ -1,15 +1,46 @@
 # coding=utf-8
+from copy import deepcopy
 from utils_main import *
 from search import *
-from hungarian import Munkres
-from copy import deepcopy
 import math
+
+# ficheiro para execução do algoritmo hungáro
+from hungarian import Munkres
+
+"""
+Implementação do puzzle Sokoban.
+ 
+Conceitos gerais:
+ 
+- Deadlock: Um estado do jogo Sokoban em que o problema deixa de ter solução
+- Posição: Tuplo com dois valores para representar o espaço cartesiano.
+"""
 
 # ______________________________________________________________________________
 # Definição de um estado do Problema
 
 
 class EstadoSokoban:
+    """ 
+    Classe para para definir um estado do puzzle Sokoban. 
+    Os métodos auxiliares vão sendo explicados ao longo do código. 
+ 
+    Atributos: 
+        - Alvos     : Lista com as posições dos alvos. 
+        - Caixas    : Lista com as posições das caixas. 
+        - Tabuleiro : Lista de listas com todos os elementos do puzzle. 
+        - Arrumador : Posição do Arrumador 
+ 
+    Tabuleiro: 
+        - Cada um das listas interiores vão ter os seguintes caracters: 
+        – ’#’   – para representar as paredes; 
+        – ’.’   – para representar as posições livres; 
+        – ’*’   – para representar as caixas; 
+        – ’o’   – (um ó minúsculo) para representar os alvos; 
+        – ’A’   – para representar o arrumador. 
+        – ’@’   – para representar uma caixa numa posição alvo. 
+        – ’B’   – para representar o arrumador em cima de uma posição alvo. 
+    """
 
     def __init__(self, tabuleiro=None, arrumador=None, caixas=None, alvos=None):
         self.alvos = list() if alvos is None else alvos
@@ -18,16 +49,33 @@ class EstadoSokoban:
         self.arrumador = tuple() if arrumador is None else arrumador
 
     def pos_livre(self, x, y):
+        """
+        Verifica se uma posição do tabuleiro está livre.
+ 
+        :param x: Coordenada x
+        :param y: Coordenada y
+        :return: bool
+        """
         try:
             return self.tabuleiro[x][y] == FREE or self.tabuleiro[x][y] == TARGET
         except IndexError:
             return False
 
     def pos_caixa(self, x, y):
+        """
+        Verifica se uma posição do tabuleiro é um caixa.
+ 
+        :param x: Coordenada x
+        :param y: Coordenada y
+        :return: bool
+        """
         try:
             return self.tabuleiro[x][y] == BOX or self.tabuleiro[x][y] == BOX_ON_TARGET
         except IndexError:
             return False
+
+    # _______________________________________
+    # Verificações de acções
 
     def ver_cima(self, x, y):
         if self.pos_livre(x - 1, y):
@@ -61,13 +109,18 @@ class EstadoSokoban:
         return (x, y) in self.alvos
 
     def __str__(self):
-        represent = ''
+        """
+        Representação em str do atributo tabuleiro. Útil para visualizar os vários estados
+ 
+        :return: str
+        """
+        represent = ""
         for line in self.tabuleiro:
             for char in line:
                 if char == USHER:
                     represent += USHER_EDIT
                 elif char == FREE:
-                    represent += ' '  
+                    represent += " "
                 elif char == TARGET:
                     represent += TARGET_EDIT
                 elif char == BOX:
@@ -80,7 +133,7 @@ class EstadoSokoban:
                     represent += USHER_ON_TARGET_EDIT
                 else:
                     represent += char
-            represent += '\n'
+            represent += "\n"
         return represent
 
     def __gt__(self, estado):
@@ -96,6 +149,7 @@ class EstadoSokoban:
         """Necessário para os algoritmos de procura em grafo."""
         return hash((line for line in self.tabuleiro))
 
+
 # ______________________________________________________________________________
 # Implementação do Problema
 
@@ -103,26 +157,12 @@ class EstadoSokoban:
 
 
 class Sokoban(Problem):
+
     def __init__(self, initial):
         """
         2.1 Formulação
-
-        Representação de um puzzle sokoban vai ser um dict em que:
-
-        - 'board': lista de listas que representa o puzzle:
-            - Cada um das listas interiores vão ter os seguintes caracters:
-            – ’#’   – para representar as paredes;
-            – ’.’   – para representar as posições livres;
-            – ’*’   – para representar as caixas;
-            – ’o’   – (um ó minúsculo) para representar os alvos;
-            – ’A’   – para representar o arrumador.
-            – ’@’   – para representar uma caixa numa posição alvo.
-            – ’B’   – para representar o arrumador em cima de uma posição alvo.
-        - 'A': posição do arrumador, tuplo xy.
-        - '*': posição das caixas, tuplo xy.
-        - 'o': posição dos alvos, tuplo xy.
-
-        :param initial: dict descrito acima
+ 
+        :param initial: estado inicial do puzzle do tipo EstadoSokoban()
         """
 
         super().__init__(initial)
@@ -130,26 +170,41 @@ class Sokoban(Problem):
         self.tabuleiro_inicial = initial.tabuleiro
         self.deadlocks = self.deadlocks_tabuleiro()
 
+    # _______________________________________
+    # Detecção de Deadlocks simples
+
     def pos_deadlock_canto(self, x, y):
         try:
             # canto superior esquerdo
-            if self.tabuleiro_inicial[x - 1][y] == WALL and self.tabuleiro_inicial[x][y - 1] == WALL and \
-                    self.tabuleiro_inicial[x - 1][y - 1] == WALL:
+            if (
+                self.tabuleiro_inicial[x - 1][y] == WALL
+                and self.tabuleiro_inicial[x][y - 1] == WALL
+                and self.tabuleiro_inicial[x - 1][y - 1] == WALL
+            ):
                 return True
 
             # canto superior direito
-            if self.tabuleiro_inicial[x - 1][y] == WALL and self.tabuleiro_inicial[x][y + 1] == WALL and \
-                    self.tabuleiro_inicial[x - 1][y + 1] == WALL:
+            if (
+                self.tabuleiro_inicial[x - 1][y] == WALL
+                and self.tabuleiro_inicial[x][y + 1] == WALL
+                and self.tabuleiro_inicial[x - 1][y + 1] == WALL
+            ):
                 return True
 
             # canto inferior esquerdo
-            if self.tabuleiro_inicial[x + 1][y] == WALL and self.tabuleiro_inicial[x][y - 1] == WALL and \
-                    self.tabuleiro_inicial[x + 1][y - 1] == WALL:
+            if (
+                self.tabuleiro_inicial[x + 1][y] == WALL
+                and self.tabuleiro_inicial[x][y - 1] == WALL
+                and self.tabuleiro_inicial[x + 1][y - 1] == WALL
+            ):
                 return True
 
             # canto inferior direito
-            if self.tabuleiro_inicial[x + 1][y] == WALL and self.tabuleiro_inicial[x][y + 1] == WALL and \
-                    self.tabuleiro_inicial[x + 1][y - 1] == WALL:
+            if (
+                self.tabuleiro_inicial[x + 1][y] == WALL
+                and self.tabuleiro_inicial[x][y + 1] == WALL
+                and self.tabuleiro_inicial[x + 1][y - 1] == WALL
+            ):
                 return True
         except IndexError:
             return False
@@ -174,14 +229,18 @@ class Sokoban(Problem):
                             auxiliar_boolean = list()
                             for i in range(canto1_y, canto2_y):
 
-                                if self.tabuleiro_inicial[canto1_x + 1][i] == WALL and \
-                                        self.tabuleiro_inicial[canto1_x][i] != WALL and \
-                                        self.tabuleiro_inicial[canto1_x][i] != TARGET:
+                                if (
+                                    self.tabuleiro_inicial[canto1_x + 1][i] == WALL
+                                    and self.tabuleiro_inicial[canto1_x][i] != WALL
+                                    and self.tabuleiro_inicial[canto1_x][i] != TARGET
+                                ):
                                     auxiliar.append((canto1_x, i))
                                     auxiliar_boolean.append(True)
-                                elif self.tabuleiro_inicial[canto1_x - 1][i] == WALL and \
-                                        self.tabuleiro_inicial[canto1_x][i] != WALL and \
-                                        self.tabuleiro_inicial[canto1_x][i] != TARGET:
+                                elif (
+                                    self.tabuleiro_inicial[canto1_x - 1][i] == WALL
+                                    and self.tabuleiro_inicial[canto1_x][i] != WALL
+                                    and self.tabuleiro_inicial[canto1_x][i] != TARGET
+                                ):
                                     auxiliar.append((canto1_x, i))
                                     auxiliar_boolean.append(True)
                                 else:
@@ -191,14 +250,18 @@ class Sokoban(Problem):
                             auxiliar = list()
                             auxiliar_boolean = list()
                             for i in range(canto1_x, canto2_x):
-                                if self.tabuleiro_inicial[i][canto1_y + 1] == WALL and \
-                                        self.tabuleiro_inicial[i][canto1_y] != WALL and \
-                                        self.tabuleiro_inicial[canto1_x][i] != TARGET:
+                                if (
+                                    self.tabuleiro_inicial[i][canto1_y + 1] == WALL
+                                    and self.tabuleiro_inicial[i][canto1_y] != WALL
+                                    and self.tabuleiro_inicial[canto1_x][i] != TARGET
+                                ):
                                     auxiliar.append((i, canto1_y))
                                     auxiliar_boolean.append(True)
-                                elif self.tabuleiro_inicial[i][canto1_y - 1] == WALL and \
-                                        self.tabuleiro_inicial[i][canto1_y] != WALL and \
-                                        self.tabuleiro_inicial[canto1_x][i] != TARGET:
+                                elif (
+                                    self.tabuleiro_inicial[i][canto1_y - 1] == WALL
+                                    and self.tabuleiro_inicial[i][canto1_y] != WALL
+                                    and self.tabuleiro_inicial[canto1_x][i] != TARGET
+                                ):
                                     auxiliar.append((i, canto1_y))
                                     auxiliar_boolean.append(True)
                                 else:
@@ -215,8 +278,11 @@ class Sokoban(Problem):
         deadlocks = list()
         for i, lista in enumerate(self.tabuleiro_inicial):
             for j in range(0, len(lista)):
-                if self.pos_deadlock_canto(i, j) and (i, j) not in self.estado_inicial.alvos and \
-                        self.tabuleiro_inicial[i][j] != WALL:
+                if (
+                    self.pos_deadlock_canto(i, j)
+                    and (i, j) not in self.estado_inicial.alvos
+                    and self.tabuleiro_inicial[i][j] != WALL
+                ):
                     if (i, j) not in deadlocks:
                         deadlocks.append((i, j))
         deadlocks_paredes = self.deadlock_parede(deadlocks)
@@ -227,11 +293,6 @@ class Sokoban(Problem):
         return deadlocks
 
     def actions(self, state):
-        """
-        comment behavior later
-        :param state:
-        :return:
-        """
 
         x, y = state.arrumador
         accoes = list()
@@ -317,36 +378,29 @@ class Sokoban(Problem):
         return EstadoSokoban(tabuleiro, arrumador, caixas, alvos)
 
     def goal_test(self, state):
-        """Return True if the state is a goal. The default method compares the
-        state to self.goal or checks for state in self.goal if it is a
-        list, as specified in the constructor. Override this method if
-        checking against a single self.goal is not enough."""
+
         for x, y in state.alvos:
             if state.tabuleiro[x][y] is not BOX_ON_TARGET:
                 return False
         return True
 
     def path_cost(self, c, state1, action, state2):
-        """Return the cost of a solution path that arrives at state2 from
-        state1 via action, assuming cost c to get up to state1. If the problem
-        is such that the path doesn't matter, this function will only look at
-        state2.  If the path does matter, it will consider c and maybe state1
-        and action. The default method costs 1 for every step in the path."""
-        # print(c)
+
         accao, direcao = action.split()
         if accao == WALK:
             return c + 1
         elif accao == PUSH:
             return c + 1
 
+
 # ______________________________________________________________________________
 # Heuristicas
-    
+
 
 def heur_euclidean_usher_target(nodo):
     """
     Distância euclidiana do arrumador ao alvo mais próximo dele
-    :param nodo:
+    :param nodo: objecto Node()
     :return:
     """
     cost = []
@@ -359,7 +413,7 @@ def heur_euclidean_usher_target(nodo):
 def heur_euclidean_usher_box(nodo):
     """
     Distância euclidiana do arrumador ao alvo mais próximo dele
-    :param nodo:
+    :param nodo: objecto Node()
     :return:
     """
     cost = []
@@ -376,7 +430,7 @@ def hung_alg_manh_usher_to_target(nodo):
     Explicação no relatório.
     """
     m = Munkres()
-        
+
     caixas = nodo.state.caixas
     alvos = nodo.state.alvos
     custo = list()
@@ -394,11 +448,7 @@ def hung_alg_manh_usher_to_target(nodo):
 
 
 def hung_alg_manh_usher_to_box(nodo):
-    """
-    Algoritmo hungaro, em que o custo de cada caixa a um alvo é a distância de manhattan.
-
-    Explicação no relatório.
-    """
+    """ Algoritmo hungaro + heur_euclidean_usher_target """
     m = Munkres()
 
     caixas = nodo.state.caixas
@@ -418,11 +468,7 @@ def hung_alg_manh_usher_to_box(nodo):
 
 
 def hung_alg_manh(nodo):
-    """
-    Algoritmo hungaro, em que o custo de cada caixa a um alvo é a distância de manhattan.
-
-    Explicação no relatório.
-    """
+    """ Algoritmo hungaro + heur_euclidean_usher_box """
     m = Munkres()
 
     caixas = nodo.state.caixas
@@ -450,13 +496,13 @@ def import_sokoban_file(filename):
     - Remove newline char
     - Verifica se cada linha tem o mesmo comprimento
 
-    :return: dict com as linhas lidas do ficheiro + posições de arrumador, caixas e alvos
+    :return: objecto do tipo EstadoSokoban()
     """
     estado = EstadoSokoban()
 
     with open(filename) as file:
         for line in file:
-            estado.tabuleiro.append(list(line.rstrip('\n')))
+            estado.tabuleiro.append(list(line.rstrip("\n")))
             for index, value in enumerate(line):
                 if value is USHER:
                     estado.arrumador = (len(estado.tabuleiro) - 1, index)
@@ -471,6 +517,6 @@ def import_sokoban_file(filename):
                     estado.arrumador = (len(estado.tabuleiro) - 1, index)
                     estado.alvos.append((len(estado.tabuleiro) - 1, index))
 
-    #len_first = len(estado.tabuleiro[0])
-    #return estado if all(len(i) == len_first for i in estado.tabuleiro) else False
+    # len_first = len(estado.tabuleiro[0])
+    # return estado if all(len(i) == len_first for i in estado.tabuleiro) else False
     return estado
